2aeec9e19511c12ee3587a8622b57266b56cf659
Christopher J Lyth <cjlyth@gmail.com>
2014-05-14 04:17:27 -0400

    added a version env variable to control the build version. This allows for previous versions to be build by setting ATOM_TAG=v0.95.0

--- build-atom.sh
+++ build-atom.sh
@@ -2,11 +2,18 @@
 
 git clone https://github.com/atom/atom.git /tmp/atom
 pushd /tmp/atom &> /dev/null
-git pull origin master
-git reset --hard HEAD
+git fetch --all
+git reset --hard ${ATOM_TAG:-master}
 echo "Compiling atom"
 ./script/build 2> /dev/null && {
 	echo "Building debian package"
-	./script/grunt mkdeb 2>/dev/null
+	./script/grunt mkdeb 2>/dev/null && {
+		find /tmp/atom-build -type f -name "*.deb" -exec cp {} /data/ \;
+	} || {
+		echo "Debian package failed!"	
+	}
+	
+} || {
+	echo "Compile failed!"
 }
-find /tmp/atom-build -type f -name "*.deb" -exec cp {} /data/ \;
+
